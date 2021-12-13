be1d4b59e90b68f714f39744d9cd83318e2f13be
Anwesha Mishra <anwesharoses@gmail.com>
2021-12-13 16:36:59 +0800

    changes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -12,19 +12,23 @@ const NewsItem=(props)=> {
                         justifyContent:'flex-end',
                         position:'absolute',
                         right:'0'
+                        
                     }}>
-                <span class=" badge rounded-pill bg-danger">{source}</span>
-                </div>
+                 <span class=" badge rounded-pill bg-danger">{source}</span>
+                    </div>
+
                  <img src={!imageurl?"https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-p3pu6l9r3p5ir2gqajjmr43o34-20180926161508.Medi.jpeg":imageurl} className="card-img-top" alt="..."/>
+
                 <div className="card-body">
-                 <h5 className="card-title">{title} </h5>  
-                <p className="card-text">{description}</p>
-                <p className="card-text"><small className="text-danger">By {!author?"Unknown":author} on {new Date(date).toUTCString()}</small></p>
+                  <h5 className="card-title">{title} </h5>  
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text-danger">By {!author?"Unknown":author} on {new Date(date).toUTCString()}</small></p>
                  <a rel="noreferrer" href={newsUrl} target="_blank" className=" btn btn-sm  btn-dark">Read More</a>
+                </div>
+
                 </div>
                 </div>
 
-            </div>
         )
     
 }
